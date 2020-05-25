5872db5fd2cba007d83d30c9f5071baeb85de297
Sasha <zzsanchozz@gmail.com>
2020-05-25 14:36:47 +0300

    change color of left section

--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,8 @@
-let rightPart = document.getElementsByClassName('team-left-column col-12 col-md-3 col-lg-3 pr-3 mt-5 hide-lg hide-md hide-sm border-bottom');
+let rightPart = document.getElementsByClassName('col-md-3');
 rightPart[0].hidden = true;
+let left = document.getElementsByClassName('js-sticky');
+left[0].style.background = '#249';
+let mid = document.getElementsByClassName('col-md-8');
+mid[0].style.background = '#249';
+let follow = document.getElementsByClassName('Box');
+follow[4].style.background = '#249';
